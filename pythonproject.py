c609770120bc19a3b516672c6b4d34e2f12b2ad1
emilydinh1 <emilydinh@tamu.edu>
2020-09-25 02:53:11 -0500

    Adds comments to my code

--- pythonproject.py
+++ pythonproject.py
@@ -29,7 +29,10 @@ lines = f.readlines()
 date_counts = {}
 months_counts = {}
 
-#
+#Loop to read each line and split based off of spaces
+#Checks to see that each line has 9 elements and then checks to see that in the third item there are at least 19 characters
+#Takes string and replaces the square bracket with a space
+#Looks at date and months
 for line in lines:
    line = line.split(" ")
    str1 = " "
@@ -40,25 +43,27 @@ for line in lines:
          date_obj = datetime.datetime.strptime(str1, '%d/%b/%Y').date()
          month_obj = date_obj.month
 
-
+         #Loops through to count how many files were made in each day
          if str1 in date_counts:
             date_counts[str1] += 1
          else:
             date_counts[str1] = 1
 
-      
+         #Loops through to count how many file were made in each month
          if month_obj in months_counts:
             months_counts[month_obj] += 1
          else:
             months_counts[month_obj] = 1
 
          
-
+#Prints out how many were made in each day and each month
 print(date_counts)
 print(months_counts)
 
+#Creates a dictionary for the files
 files = {}
 
+#Checks to see that each line has 9 items then checks to see that in the 6th item, there are at least 3 characters
 for line in lines:
    line = line.split(" ")
    str1 = " "
@@ -67,28 +72,32 @@ for line in lines:
          str1 = line[6].replace('"', '').replace('>', '')
          #if str1 == 'index.html':
          #   print(str1)
+         
+         #Loops to count different types of files
          if str1 in files:
             files[str1] += 1
          else:
             files[str1] = 1
 
-
+#Sorts files to take first and last element for the least and most requested files
 #print(files)
 sorted_files = sorted(files)
 print("The least requested file:", sorted_files[0])
 print("The most requested file:", sorted_files[len(sorted_files)-1])
 
-
+#Declares variables 
 success_requests = 0
 redirected_requests = 0
 
+
+#Reads through each line and if there is anytime 4xx (x being wildcards) then it adds to counter
 for line in lines:   
    success_requests +=1
    for i in range(400, 499):
       if line.find(str(i)) != -1:
          success_requests += 1
 
-
+#Reads through each line and if there is anytime 3xx (x being wildcards) then it adds to counter
 for line in lines:   
    redirected_requests +=1
    for i in range(300, 399):
@@ -103,12 +112,16 @@ for line in lines:
    if line.find("1995") != -1:
       totalrequests_lastyear += 1
 
+
+#Declares my variables
+#Also finds the percentage of successful and redirected requests out of total requests
 percentsuc = 0
 percentsuc = success_requests / totalrequests
 percentredir = 0
 percentredir = redirected_requests / totalrequests
 
 f.close() 
+#Prints out information
 print("\nLast Year Requests: ", totalrequests_lastyear)
 print("Total Requests: ", totalrequests)
 print("Successful requests with 4xx status code in percentage:  ", (percentsuc))
